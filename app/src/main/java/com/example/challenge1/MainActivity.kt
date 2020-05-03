5d7caa95ca7b44855fa7832fe7fbb5a7141deda0
RizkiKarianata <natarizky884@gmail.com>
2020-05-03 15:48:14 +0700

    Add method kotlin android extension

--- app/src/main/java/com/example/challenge1/MainActivity.kt
+++ app/src/main/java/com/example/challenge1/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.challenge1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
