1b10dd1754103ad49a07fe79fe0bc57f50aef461
RizkiKarianata <natarizky884@gmail.com>
2020-05-03 15:21:10 +0700

    Add new activity

---
+++ app/src/main/java/com/example/challenge1/ProfilActivity.kt
@@ -0,0 +1,12 @@
+package com.example.challenge1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ProfilActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profil)
+    }
+}
